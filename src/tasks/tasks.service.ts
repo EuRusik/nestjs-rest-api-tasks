1b120c56ddc72a56dcffd04fa10617f4116fa53e
EuRusik <john.rusakov@gmail.com>
2020-06-04 19:32:48 +0300

    remove unuse code

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,31 +18,6 @@ export class TasksService {
       return this.taskRepository.getTasks(createTaskDto);
     }
   }
-  //
-  // getTasksByFilter(filterDto: GetTasksFilterDto): ITask[] {
-  //   const { status, search } = filterDto;
-  //   let tasks = this.getAllTasks();
-  //   if (status) {
-  //      tasks = this.getTasksByStatus(status);
-  //   }
-  //   if (search) {
-  //     tasks = this.getTasksBySearch(search);
-  //   }
-  //   return tasks;
-  // }
-  //
-  // private getTasksByStatus(status: TaskStatus): ITask[] {
-  //   if (status) {
-  //     return this.tasks.filter((task) => task.status === status);
-  //   }
-  // }
-  //
-  // private getTasksBySearch(search: string): ITask[] {
-  //   if (search) {
-  //     return this.tasks.filter((task) => task.title.includes(search) || task.description.includes(search))
-  //   }
-  // }
-  //
 
   async getTaskById(id: number): Promise<TaskEntity> {
     if (id) {
@@ -52,15 +27,7 @@ export class TasksService {
       } return found;
     }
   }
-  // getTaskById(id: string): ITask {
-  //   if (id) {
-  //     const found = this.tasks.find((task) => task.id === id);
-  //     if (!found) {
-  //       throw new NotFoundException(`Task with ${id} not found!`);
-  //     } return found;
-  //   }
-  // }
-  //
+
   async createTask(createTaskDto: CreateTaskDto): Promise<TaskEntity> {
     if (createTaskDto) {
       return this.taskRepository.createTask(createTaskDto);
@@ -75,16 +42,7 @@ export class TasksService {
       }
     }
   }
-  //
-  // private findIndexTaskById(id: string): number {
-  //   if (id) {
-  //     const index = this.tasks.findIndex((task) => task.id === id);
-  //     if (index === -1) {
-  //       throw new NotFoundException(`Task with ${id} not found!`);
-  //     } return index;
-  //   }
-  // }
-  //
+
   async updateTaskStatus(id: number, status: TaskStatus): Promise<TaskEntity> {
     if (id && status) {
       const task = await this.getTaskById(id);
