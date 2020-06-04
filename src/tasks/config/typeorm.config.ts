fc1043505b50e7fea62d53fba94b2bd32f39bd97
EuRusik <john.rusakov@gmail.com>
2020-06-04 15:38:45 +0300

    add orm config

---
+++ src/tasks/config/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ACCESSES } from '../../../accesses.config';
+
+export function typeOrmConfig(): TypeOrmModuleOptions {
+  return {
+    type: 'mysql',
+    host: ACCESSES.DB.HOST,
+    port: ACCESSES.DB.PORT,
+    username: ACCESSES.DB.USERNAME,
+    password: ACCESSES.DB.PASSWORD,
+    database: ACCESSES.DB.DB_NAME,
+    entities: [__dirname + '/../**/*.entity.ts'],
+    synchronize: true
+  }
+}
